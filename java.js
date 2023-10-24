60fadc730d0d61d4540a5089f33021aaa5297bd3
Labonno Khan Bonna <bonnalabonnokhan2708@gmail.com>
2023-10-24 19:44:01 +0600

    my second commit

---
+++ java.js
@@ -0,0 +1,28 @@
+const person = {
+    name : "labonno",
+    age: 25,
+    address : "Dhaka",
+    phone:"01689763403"
+}
+
+// //dot notation
+// console.log(person.age)
+// console.log(person["phone"])
+
+// const x = "address";
+// console.log(person[x])
+
+const keys  = Object.keys(person)
+const values = Object.values(person)
+// console.log(keys)
+// console.log(values)
+
+for (let key of keys )
+{
+    console.log(person[key])
+}
+
+for (let i =0 ; i<keys; i++)
+{
+    console.log(person[keys[i]])
+}
